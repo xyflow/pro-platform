6d2bcaf333a2486912dacc8ba13d9726e012231b
Christopher Möller <chrtze@gmail.com>
2023-11-27 22:12:22 +0100

    feat(examples): fetch examples from static json instead of github api (#25)
    * feat(examples): fetch examples from static json instead of github api
    
    * chore(examples): add comment

--- src/components/ProExampleViewer/tabs/preview.tsx
+++ src/components/ProExampleViewer/tabs/preview.tsx
@@ -2,8 +2,8 @@ import { Framework } from '@/types';
 
 function ProExamplePreview({ exampleId, frameworkId }: { exampleId: string; frameworkId: Framework }) {
   return (
-    <div className="my-4 relative min-h-[400px] h-[60vh] max-h-[550px] rounded-sm overflow-hidden border border-gray-200">
-      <iframe className="w-full h-full" src={`https://pro-example-apps.vercel.app/${exampleId}`} />
+    <div className="my-4 relative min-h-[400px] h-[75vh] max-h-[650px] rounded-sm overflow-hidden border border-gray-200">
+      <iframe className="w-full h-full" src={`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/${exampleId}`} />
     </div>
   );
 }
