74cc43e19414ed2917f3d4eca4f08598d360ecb0
Christopher Möller <chrtze@gmail.com>
2023-11-29 21:01:45 +0100

    feat(nextjs): configure caching to always have the updated examples

--- src/app/(dashboard)/examples/[framework]/page.tsx
+++ src/app/(dashboard)/examples/[framework]/page.tsx
@@ -19,3 +19,9 @@ export default async function ProExamplesOverview({ params }: { params: { framew
     </div>
   );
 }
+
+export async function generateStaticParams() {
+  return [{ framework: 'react' }];
+}
+
+export const dynamicParams = false;
