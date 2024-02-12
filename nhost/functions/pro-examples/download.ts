b59a38de406daca912c3bc699c6b5a08bee25829
Hayleigh Thompson <me@hayleigh.dev>
2024-02-12 17:04:48 +0000

    :sparkles: Add the ability to serve pro examples locally instead of from redis.

--- nhost/functions/pro-examples/download.ts
+++ nhost/functions/pro-examples/download.ts
@@ -2,9 +2,19 @@ import { Request, Response } from 'express';
 import { isSubscribed } from '../_utils/graphql/subscriptions';
 import { authPost } from '../_utils/middleware';
 import { redis } from '../_utils/redis';
+import { readFile, readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { join, relative } from 'node:path';
+import { existsSync } from 'node:fs';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 async function downloadProExample(req: Request, res: Response, { userId }: { userId: string }) {
   const { id, framework } = req.body;
+
+  if (process.env.NODE_ENV === 'development') {
+    return downloadLocalProExample(req, res);
+  }
 
   if (!id || !framework || !userId) {
     return res.status(500).send({ message: 'Bad request.' });
@@ -29,3 +39,35 @@ async function downloadProExample(req: Request, res: Response, { userId }: { use
 }
 
 export default authPost(downloadProExample);
+
+type ProExampleContent = {
+  timestamp: number;
+  files: { path: string; content: string }[];
+};
+
+async function downloadLocalProExample(req: Request, res: Response) {
+  const { id } = req.body;
+
+  if (!existsSync(join(__dirname, './_examples', id))) {
+    return res.status(500).send({ message: 'Example does not exist.' });
+  }
+
+  try {
+    const payload = { timestamp: Date.now(), files: [] } as ProExampleContent;
+    const src = join(__dirname, './_examples', id, 'app');
+
+    for (const file of await readdir(src, { withFileTypes: true, recursive: true })) {
+      if (!file.isFile()) continue;
+
+      const relativePath = relative(src, file.path);
+      const path = relativePath === '' ? `/${file.name}` : `/${relativePath}/${file.name}`;
+      const content = await readFile(join(file.path, file.name), 'utf-8');
+
+      payload.files.push({ path, content });
+    }
+
+    return res.status(200).json(payload);
+  } catch {
+    return res.status(500).send({ message: 'Bad request.' });
+  }
+}
