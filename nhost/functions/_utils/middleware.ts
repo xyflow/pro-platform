0fcf3bf6655cc4a7088f1c3df5ed2cfa1821b570
Hayleigh Thompson <me@hayleigh.dev>
2024-02-14 14:26:29 +0000

    :sparkles: Add a middleware for non-authed nhost functions .

--- nhost/functions/_utils/middleware.ts
+++ nhost/functions/_utils/middleware.ts
@@ -31,3 +31,36 @@ export const authPost = (fn: any) => async (req: Request, res: Response) => {
     return res.status(500).send({ message: 'Internal server error.' });
   }
 };
+
+export const post = (fn: any) => async (req: Request, res: Response) => {
+  // In development it's OK to allow access from any origin, but when we deploy
+  // to production it's important to only allow access from the pro platform's
+  // URL.
+  //
+  // This is because functions that use this middleware are public and not behind
+  // any authentication.
+  const allowedOrigin = process.env.NODE_ENV === 'development' ? '*' : process.env.PRO_PLATFORM_URL;
+
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Method not allowed.' });
+  }
+
+  try {
+    return await fn(req, res);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: 'Internal server error.' });
+  }
+};
