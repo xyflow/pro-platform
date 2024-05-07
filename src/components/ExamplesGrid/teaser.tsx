9b81ddf21c24aebadbede52be34e5ec8dbd819ff
Christopher Möller <chrtze@gmail.com>
2024-05-07 17:37:53 +0200

    style(support-page): adjust text colors

--- src/components/ExamplesGrid/teaser.tsx
+++ src/components/ExamplesGrid/teaser.tsx
@@ -17,29 +17,29 @@ export default function ExampleTeaser({ example }: ExampleTeaserProps) {
   const showFreeLabel = example.free && !isSubscribed;
 
   return (
-    <Card className="flex flex-col">
-      <div className="relative h-[200px]">
-        <Image
-          alt="Example Teaser"
-          fill
-          src={`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/${example.id}/thumbnail.jpg`}
-          className="object-cover"
-          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-        />
-      </div>
-      <CardHeader className="flex-1">
-        <CardTitle className="flex items-center gap-x-1.5">
-          {example.name ?? example.id} {showFreeLabel && <Pill>free</Pill>}
-        </CardTitle>
-        {example.description && <CardDescription>{example.description}</CardDescription>}
-        <Link
-          className={cn('!mt-auto text-react font-bold items-center hover:text-slate-800 flex pt-4 w-full')}
-          href={`/examples/${example.framework}/${example.id}`}
-        >
-          View Example
-          <ArrowLongRightIcon className="ml-1 h-6 w-6" />
-        </Link>
-      </CardHeader>
-    </Card>
+    <Link href={`/examples/${example.framework}/${example.id}`}>
+      <Card className="flex flex-col">
+        <div className="relative h-[200px]">
+          <Image
+            alt="Example Teaser"
+            fill
+            src={`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/${example.id}/thumbnail.jpg`}
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+          />
+        </div>
+        <CardHeader className="flex-1">
+          <CardTitle className="flex items-center gap-x-1.5">
+            {example.name ?? example.id} {showFreeLabel && <Pill>free</Pill>}
+          </CardTitle>
+          {example.description && <CardDescription>{example.description}</CardDescription>}
+
+          <div className="!mt-auto text-react font-bold items-center hover:text-slate-800 flex pt-4 w-full">
+            View Example
+            <ArrowLongRightIcon className="ml-1 h-6 w-6" />
+          </div>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 }
