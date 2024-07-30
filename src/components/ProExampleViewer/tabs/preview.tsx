b4a7e6ede31c1c2a5bbc99087cba6a296a7a682c
moklick <info@moritzklack.com>
2024-07-30 14:49:22 +0200

    chore(examples): add new tab button

--- src/components/ProExampleViewer/tabs/preview.tsx
+++ src/components/ProExampleViewer/tabs/preview.tsx
@@ -1,10 +1,19 @@
+import { Button } from '@xyflow/xy-ui';
+
 import { Framework } from '@/types';
 
 function ProExamplePreview({ exampleId, frameworkId }: { exampleId: string; frameworkId: Framework }) {
+  const iframeLink = `${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/${exampleId}`;
+
   return (
-    <div className="my-4 relative min-h-[400px] h-[75vh] max-h-[650px] rounded-sm overflow-hidden border border-gray-200">
-      <iframe className="w-full h-full" src={`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/${exampleId}`} />
-    </div>
+    <>
+      <div className="mt-4 mb-2relative min-h-[400px] h-[75vh] max-h-[650px] rounded-sm overflow-hidden border border-gray-200">
+        <iframe className="w-full h-full" src={iframeLink} />
+      </div>
+      <a target="_blank" href={iframeLink}>
+        <Button variant="link">Open example in a new tab</Button>
+      </a>
+    </>
   );
 }
 
