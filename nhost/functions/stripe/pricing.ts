0d5b77ed9b03cedad14e13cd4ef8d45803437785
Christopher Möller <chrtze@gmail.com>
2024-08-19 16:43:32 +0200

    chore(nhost): remove public pricing route

--- nhost/functions/stripe/pricing.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { getPrices } from '../_utils/stripe';
-
-export default async function getPricingInfo(req: Request, res: Response) {
-  const prices = await getPrices();
-
-  return res.status(200).send(prices);
-}
