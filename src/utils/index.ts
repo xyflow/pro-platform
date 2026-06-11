16edd20a20fe1a32b1c70e3a926a8fd18833f4f4
Christopher Möller <chrtze@gmail.com>
2026-06-11 12:27:11 +0200

    fix hostname protocol

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -47,6 +47,6 @@ export function getHostName() {
   }
 
   return process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    ? `http://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
     : 'http://localhost:3000';
 }
